defeb222d313e57a42306bd8d20b9df43c4b5d7e
Kerry Patrick <kpatrick@mmmicrosystems.com>
2023-11-25 10:21:10 -0600

    end of chapter 1

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -4,12 +4,24 @@ import { act } from "react-dom/test-utils";
 import { Appointment } from "../src/Appointment";
 
 describe("Appointment", () => {
+  let container;
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.replaceChildren(container);
+  });
+
+  const render = (component) =>
+    act(() => ReactDOM.createRoot(container).render(component));
+
   it("renders the customer first name", () => {
     const customer = { firstName: "Ashley" };
-    const component = <Appointment customer={customer} />;
-    const container = document.createElement("div");
-    document.body.appendChild(container);
-    act(() => ReactDOM.createRoot(container).render(component));
+    render(<Appointment customer={customer} />);
     expect(document.body.textContent).toContain("Ashley");
+  });
+
+  it("renders any customer first name", () => {
+    const customer = { firstName: "Jordan" };
+    render(<Appointment customer={customer} />);
+    expect(document.body.textContent).toContain("Jordan");
   });
 });
