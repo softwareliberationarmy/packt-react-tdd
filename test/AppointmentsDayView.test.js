89ca2003dd30cb64e0930b085fc7240657e4dc68
Kerry Patrick <kpatrick@mmmicrosystems.com>
2023-11-25 15:44:06 -0600

    renaming Appointment files

--- test/AppointmentsDayView.test.js
+++ test/AppointmentsDayView.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { act } from "react-dom/test-utils";
-import { Appointment, AppointmentsDayView } from "../src/Appointment";
+import { Appointment, AppointmentsDayView } from "../src/AppointmentsDayView";
 
 describe("Appointment", () => {
   let container;
